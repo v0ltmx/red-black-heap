954cb89da66857f36b916df7752d4686092cdd13
v0ltmx <lucas2015.gabrill@hotmail.com>
2021-08-30 14:18:37 -0300

    binary search fix

--- code/Arvore-rubro-negra.js
+++
@@ -1,4 +0,0 @@
-// Implemente um árvore rubro-negra contendo as operações de inserir e remover nós.
-// Grave um vídeo explicando a sua implementação. No seu vídeo dê destaque as regras para inserção e remoção de nós.
-// Demonstre todos os casos possíveis (tanto para inserção quanto para remoção)
-
