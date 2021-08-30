bedca6a4cd79072136f9cacd47452407ef61ffca
v0ltmx <lucas2015.gabrill@hotmail.com>
2021-08-30 14:19:58 -0300

    files fixed

--- code/Arvore-binaria-busca.js
+++ code/Arvore-binaria-busca.js
@@ -1,7 +1,4 @@
-// Implemente um árvore rubro-negra contendo as operações de inserir e remover nós.
-// Grave um vídeo explicando a sua implementação. No seu vídeo dê destaque as regras para inserção e remoção de nós.
-// Demonstre todos os casos possíveis (tanto para inserção quanto para remoção)
-
+//Implementação Árvore Binária de Busca
 class Node{
     constructor(valor, esquerda, direita){
         this.valor = valor;
